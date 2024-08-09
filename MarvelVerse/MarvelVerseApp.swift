b9efcae899bc55656f9bc292c6cc21c98d78e1f8
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-09 21:07:47 +0200

    Initial Commit

---
+++ MarvelVerse/MarvelVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelVerseApp.swift
+//  MarvelVerse
+//
+//  Created by Mateusz Krówczyński on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
