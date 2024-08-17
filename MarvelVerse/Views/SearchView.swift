39ec94cae0c3e98c94bff5561443fdc11a4ce90d
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-17 15:41:46 +0200

    feat
    added working template for search view

--- MarvelVerse/Views/SearchView.swift
+++ MarvelVerse/Views/SearchView.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct SearchView: View {
+
+    @State private var query: String = ""
+    @StateObject private var viewModel: MarvelVerseViewModel = MarvelVerseViewModel()
+    
     var body: some View {
         ZStack {
             Color.marvelWhite.ignoresSafeArea()
-            VStack {
-                HeaderView()
+            VStack(spacing: 8){
+                if viewModel.isLoadingSearch {
+                    ProgressView()
+                        .tint(.marvelRed)
+                        .background(
+                            UnevenRoundedRectangle(
+                                topLeadingRadius: 8,
+                                topTrailingRadius: 8
+                            )
+                        )
+                        .offset(y: -10)
+                } else {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .font(.title)
+                            .foregroundStyle(.marvelRed)
+                            .padding(.leading)
+                            .padding(.trailing, 0)
+                        TextField("Search Movies, TV Series...", text: $query) {
+                            Task {
+                                viewModel.getSearchDBData(query:query)
+                            }
+                        }
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.trailing,12)
+                        .padding(.leading, 0)
+                        .frame(maxWidth: .infinity)
+                        .tint(.marvelWhite)
+                        
+                        if !query.isEmpty {
+                            Image(systemName: "xmark.circle.fill")
+                                .padding(.trailing,24)
+                                .background(Color.marvelBlack.opacity(0.01))
+                                .foregroundStyle(.marvelBlack)
+                                .onTapGesture {
+                                    query = ""
+                                    Task {
+                                        viewModel.getSearchDBData(query:query)
+                                    }
+                                }
+                        }
+                    }
+                    .padding(.top)
+                    .background(
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 8,
+                            topTrailingRadius: 8
+                        )
+                    )
+                    .offset(y: -10)
+                }
                 Spacer()
             }
         }
