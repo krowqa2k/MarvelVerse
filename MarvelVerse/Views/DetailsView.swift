e97c90634db5ff82e15b931c07211697a15e0909
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-18 14:43:45 +0200

    feat
    Created Detail View

--- MarvelVerse/Views/DetailsView.swift
+++ MarvelVerse/Views/DetailsView.swift
@@ -8,8 +8,89 @@
 import SwiftUI
 
 struct DetailsView: View {
+    
+    @StateObject private var viewModel = MarvelVerseViewModel()
+    var imageName: String = Constants.mockImage
+    var comic: Comic = .mock
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.marvelWhite.ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 26){
+                HStack(alignment: .top, spacing: 14){
+                    ImageLoader(imageURL: imageName)
+                        .frame(width: 130, height: 200)
+                        .cornerRadius(4)
+                    
+                    VStack(alignment: .leading, spacing: 8){
+                        Text(comic.title ?? "")
+                            .foregroundStyle(.marvelBlack)
+                            .font(.headline)
+                        
+                        Text(comic.series?.name ?? "")
+                            .foregroundStyle(.marvelBlack.opacity(0.5))
+                            .font(.caption)
+                        
+                        Image("marvel_comics")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        
+                        Rectangle()
+                            .fill(Color.marvelRed)
+                            .frame(maxWidth: 180)
+                            .frame(height: 80)
+                            .cornerRadius(8)
+                            .overlay {
+                                Link("READ NOW", destination: {
+                                    if let urlString = viewModel.extractURL(data: comic.urls ?? [["":""]]),
+                                       let url = URL(string: urlString) {
+                                        return url
+                                    } else {
+                                        return URL(string: "https://www.google.com")!
+                                    }
+                                }())
+                                .tint(.marvelWhite)
+                            }
+                    }
+                }
+                
+                VStack(alignment: .leading, spacing: 4){
+                    Text("CREATORS")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.marvelBlack)
+                    
+                    HStack(spacing: 4){
+                        if let comicCreators = comic.creators?.items {
+                            ForEach(comicCreators, id: \.resourceURI){ creator in
+                                HStack(spacing: 4){
+                                    Text("*'\(creator.name ?? "")*'")
+                                        .font(.subheadline)
+                                        .foregroundStyle(.marvelBlack)
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                
+                VStack(alignment: .leading, spacing: 4){
+                    Text("SYNOPSIS")
+                        .font(.headline)
+                        .foregroundStyle(.marvelRed)
+                    
+                    Text(comic.description ?? "")
+                        .foregroundStyle(.marvelBlack.opacity(0.9))
+                        .font(.callout)
+                        .multilineTextAlignment(.leading)
+                        .padding(.horizontal)
+                        .frame(width: UIScreen.main.bounds.width - 20)
+                }
+                Spacer()
+            }
+            .padding(.top, 80)
+        }
+        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
