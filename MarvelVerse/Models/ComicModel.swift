03ea15ccb8ec5643dc30a1e909f4384940f8c14a
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-18 17:29:45 +0200

    bug fix
    updated model with correct structure

--- MarvelVerse/Models/ComicModel.swift
+++ MarvelVerse/Models/ComicModel.swift
@@ -24,41 +24,48 @@ struct Comic: Codable, Identifiable {
     let series: Series?
     let creators: Creators?
     let thumbnail: [String:String]
-
     
     static var mock: Comic {
         Comic(
             id: 59551,
             title: "Wolverine (2024) #2",
-            description: "WHERE GOES THE WENDIGO?! Who stalks WOLVERINE in the Canadian North? And what mysterious designs does the WENDIGO have on the Best There Is? Logan just wants to be left alone, but a war on two fronts will evolve with an unexpected turn! Don't miss the debut of the all-new Wendigo, as the secret it hides will shape Wolverine's mission…",
+            description: "WHERE GOES THE WENDIGO?!...",
             urls: [["detail": "http://marvel.com/comics/issue/113524/wolverine_2024_2?utm_campaign=apiRef&utm_source=2b23e25a76e180235beae440efbd1949"]],
-            series: Series.init(resourceURI: "http://gateway.marvel.com/v1/public/series/38960", name: "Wolverine (2024 - Present)"),
+            series: Series(resourceURI: "http://gateway.marvel.com/v1/public/series/38960", name: "Wolverine (2024 - Present)"),
             creators: Creators(
-                available: "5",
-                returned: "5",
+                available: 5,
                 collectionURI: "http://gateway.marvel.com/v1/public/comics/113524/creators",
-                items: [CreatorsItem(
-                    resourceURI: "http://gateway.marvel.com/v1/public/creators/13081",
-                    name: "Saladin Ahmed",
-                    role: "writer"
-                ),
-                CreatorsItem(
-                    resourceURI: "http://gateway.marvel.com/v1/public/creators/12313",
-                    name: "Mark Basso",
-                    role: "editor"
-                )]
+                items: [
+                    CreatorsItem(
+                        resourceURI: "http://gateway.marvel.com/v1/public/creators/13081",
+                        name: "Saladin Ahmed",
+                        role: "writer"
+                    ),
+                    CreatorsItem(
+                        resourceURI: "http://gateway.marvel.com/v1/public/creators/12313",
+                        name: "Mark Basso",
+                        role: "editor"
+                    )
+                ],
+                returned: 5
             ),
             thumbnail: ["path": "http://i.annihil.us/u/prod/marvel/i/mg/1/30/56538fd257915"]
         )
     }
 }
 
+
 struct Creators: Codable {
-    let available, returned, collectionURI: String?
+    let available: Int?
+    let collectionURI: String?
     let items: [CreatorsItem]?
+    let returned: Int?
 }
 
 struct CreatorsItem: Codable {
-    let resourceURI, name, role: String?
+    let resourceURI: String?
+    let name: String?
+    let role: String?
 }
+
 
